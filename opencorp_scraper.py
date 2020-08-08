dea158fdd6dd9bc6c91757be77dc09b88bb099d9
Gergely Attila Kiss <corra971407@gmail.com>
2020-08-08 12:22:22 +0200

    generalize scraper to be an easily callable function

--- opencorp_scraper.py
+++ opencorp_scraper.py
@@ -1,27 +1,25 @@
 from selenium import webdriver
-from bs4 import BS
+from bs4 import BeautifulSoup as BS
 import csv
 
-link = "https://opencorporates.com/companies/sk?q=&utf8=%E2%9C%93"
-
 class CountryScraper(object):
-	def __init__(self):
-		self.driver = webdriver.Chrome()
-		#use whatever driver engine you have in path (e.g.: Firefox, IE, Safari, Opera)
+    def __init__(self,country,page_number):
+        self.driver = webdriver.Chrome()
+        self.country = country
+        self.page_number = page_number
+        #use whatever driver engine you have in path (e.g.: Firefox, IE, Safari, Opera)
 
-	def scrape_country(self):
-		self.driver.get(link)
-		soup = BS(self.driver.page_source, 'html.parser')
-		writer = csv.DictWriter(open('where_sample.csv', 'w'), fieldnames = 'name country'.split(' '))
-			for firm in soup.find_all('a', {'class':'company_search_result'}):
-				title = firm.get('title').split(' ',7)[7]
-				name  = title.split('(', 1)[0][1:-2]
-    			country = title.split('(',1)[1][0:8]
-    			dictionary = {'name': name, 'country': country}
-    			wirter.writerow(dictionary)
-		self.driver.close()
-
-	def scrape(self):
-		self.scrape_country()
-
-		#scrapethissite.com
+    def scrape(self):
+        self.driver.get("https://opencorporates.com/companies/"+self.country+"?page="+self.page_number+"&q=&utf8=%E2%9C%93")
+        #open page with a driver
+        soup = BS(self.driver.page_source, 'html.parser')
+        writer = csv.DictWriter(open(self.country+self.page_number+'.csv', 'w'), fieldnames = 'name country'.split(' '))
+        writer.writeheader()
+        for firm in soup.find_all('a', {'class':'company_search_result'}):
+            title = firm.get('title').split(' ',7)[7]
+            name  = title.split('(', 1)[0][1:-2]
+            country = title.split('(',1)[1][0:8]
+            dictionary = {'name': name, 'country': country}
+            #get the name of the firm and the country it is in
+            writer.writerow(dictionary)
+        self.driver.close()

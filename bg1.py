385cf0725717bc8d00d58144d35f82d2fb39de97
Gergely Attila Kiss <corra971407@gmail.com>
2020-08-10 09:27:32 +0200

    delete bg

--- bg1.py
+++
@@ -1,4 +0,0 @@
-import opencorp_scraper as scraper
-
-bg1 = scraper.CountryScraper('bg','1')
-bg1.scrape()
--- bg2.py
+++
@@ -1,4 +0,0 @@
-import opencorp_scraper as scraper
-
-bg2 = scraper.CountryScraper('bg','2')
-bg2.scrape()
